b8672131d804ab59d3efbed62c9e7396531f471b
IssahBen <benedictissah@gmail.com>
2024-02-08 17:21:36 -0600

    optimized

---
+++ twosum.rb
@@ -0,0 +1,22 @@
+
+ddef two_sum(nums, target)
+indices=[]
+  nums.each_with_index do |x,i|
+      index=i+1
+      while index < nums.size
+          if x+ nums[index] == target
+          indices=[i,index].sort
+
+            
+      
+          end  
+            index +=1
+      end
+
+  end
+
+   
+
+nums=[3,3]
+
+p two_sum(nums,6)
