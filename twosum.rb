740faac4cf12ca1978164f5f34bceaae6cdacf8a
IssahBen <benedictissah@gmail.com>
2024-02-08 17:26:11 -0600

    code corrected

--- twosum.rb
+++ twosum.rb
@@ -1,22 +1,24 @@
 
 def two_sum(nums, target)
-indices=[]
-  nums.each_with_index do |x,i|
-      index=i+1
-      while index < nums.size
-          if x+ nums[index] == target
-          indices=[i,index].sort
-
-            
-      
-          end  
-            index +=1
+    indices=[]
+      nums.each_with_index do |x,i|
+          index=i+1
+          while index < nums.size
+              if x+ nums[index] == target
+              indices=[i,index].sort
+  
+                
+          
+              end  
+                index +=1
+          end
+    
       end
-
+  
+       
+    
+  
+      return indices
+  
   end
-
    
-
-nums=[3,3]
-
-p two_sum(nums,6)
